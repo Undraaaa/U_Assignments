7c263d101dd32ac04e9909985d222b54a999801b
Undral Lkhagva <undraa188@gmail.com>
2022-09-02 22:22:02 +0900

    Adding Q1

--- Section3/Task1.py
+++ Section3/Task1.py
@@ -4,16 +4,38 @@
 # Created by Undral, Aug, 2022
 
 ''' Task-1 '''
+import os
+os.getcwd() # get current working directory 
+#os.chdir(r"Aug27_lec3")
+
+import pandas as pd
+import numpy as np
 
 '''
 Q1. Add rows with arbitrary values to "data.xlsx" so that you have 50 rows in total 
 '''
+df_data = pd.read_excel("3_Data_table/data/data.xlsx")
+df_data1= pd.read_excel("3_Data_table/data/data.xlsx")
+df_data2= pd.read_excel("3_Data_table/data/data.xlsx")
+df_data3= pd.read_excel("3_Data_table/data/data.xlsx")
+df_data4= pd.read_excel("3_Data_table/data/data.xlsx")
+type(df_data1)
 
+df_data = df_data.append(df_data1, ignore_index = True)
+df_data = df_data.append(df_data2, ignore_index = True)
+df_data = df_data.append(df_data3, ignore_index = True)
+df_data = df_data.append(df_data4, ignore_index = True)
+print(df_data)
 
+    '''
+    for i in range(1,4):
+        df_data = df_data.append(df_data{i}, ignore_index = True)
+    '''
 
 '''
 Q2. Create a '.csv' file with data of only women older than 25 years old
 '''
+
 
 
 '''
