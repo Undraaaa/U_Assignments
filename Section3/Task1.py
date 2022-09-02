6294bca6c4482e7968dce20d552dadb52e48eb86
Undral Lkhagva <undraa188@gmail.com>
2022-09-02 21:15:21 +0900

    Add Section3

---
+++ Section3/Task1.py
@@ -0,0 +1,21 @@
+# Introduction-Python training
+# Lecture 3: Data Types: Numpy and Pandas
+# Assignment: Task1
+# Created by Undral, Aug, 2022
+
+''' Task-1 '''
+
+'''
+Q1. Add rows with arbitrary values to "data.xlsx" so that you have 50 rows in total 
+'''
+
+
+
+'''
+Q2. Create a '.csv' file with data of only women older than 25 years old
+'''
+
+
+'''
+Q3. Create a '.json' file with data men under 23 years old
+'''
---
+++ Section3/Task2.py
@@ -0,0 +1,24 @@
+# Introduction-Python training
+# Lecture 3: Data Types: Numpy and Pandas
+# Assignment: Task1
+# Created by Undral, Aug, 2022
+
+''' Task-2 '''
+
+'''
+Q1. What are the values in row 17 and column 2-5 of dataframe created frim "data.xlsx"
+'''
+
+'''
+Q2. What are the values row 25-28 and column 'firstName, age' of dataframe created from "data.xlsx"
+'''
+
+
+'''
+Q3. Find the lowest, the highest and mean salary and age for men and women seprately using 'groupby'
+'''
+
+
+'''
+Q4. Find the lowest, the highest and mean salary and for men and women separately using 'pivot table'
+'''
